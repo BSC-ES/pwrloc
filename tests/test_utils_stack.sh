cd81ac658d5cb8921aa3a31a6c960bd6c57ad269
Okke van Eck <okke.vaneck@bsc.es>
2025-11-19 17:53:52 +0100

    Changed values of default stack to reflect index. Implemented the check for destroy

--- tests/test_utils_stack.sh
+++ tests/test_utils_stack.sh
@@ -7,6 +7,24 @@
 . "$SRCDIR/utils/stack_utils.sh"
 
 
+# Setup function to be used for setting up a stack uniformly after the internal
+# functions have been verified separately.
+#   Usage:      _setup_test_stack <stack_name>
+_setup_test_stack() {
+    # Setup stack.
+    stack_create "$1"
+    stack_push "$1" "val0"
+    stack_push "$1" "val1"
+    stack_push "$1" "val2"
+
+    # Make sure the starting length is correct.
+    if [ "$(stack_len "$1")" -ne 3 ]; then
+        printf "<_setup_test_stack> Created stack does not have length 3.\n" >&2
+        return 1
+    fi
+}
+
+
 # Test stack_exist and stack_create.
 test_stack_creation() {
     # Test if stack_exists correctly detects non-existing stacks.
@@ -44,12 +62,12 @@ test_stack_push() {
     fi
 
     # Check stack length after two pushes.
-    stack_push "stack" "val"
+    stack_push "stack" "val0"
     if [ "$(stack_len "stack")" -ne 1 ]; then
         printf "Pushing a value does not increase stack length to 1.\n" >&2
         return 1
     fi
-    stack_push "stack" "val2"
+    stack_push "stack" "val1"
     if [ "$(stack_len "stack")" -ne 2 ]; then
         printf "Pushing a value does not increase stack length to 2.\n" >&2
         return 1
@@ -58,23 +76,14 @@ test_stack_push() {
 
 # Test stack_pop.
 test_stack_pop() {
-    # Setup stack.
-    stack_create "stack"
-    stack_push "stack" "1"
-    stack_push "stack" "2"
-    stack_push "stack" "3"
-
-    # Make sure the starting length is correct.
-    if [ "$(stack_len "stack")" -ne 3 ]; then
-        printf "Created stack does not have length 3.\n" >&2
-        return 1
-    fi
+    # Setup default stack.
+    _setup_test_stack "stack"
 
     # Check for first pop.
     stack_pop "stack"
     top_val=$STACK_POP_RESULT
-    if [ "$top_val" != "3" ]; then
-        printf "Top value incorrect: '%s' != 3.\n" "$top_val" >&2
+    if [ "$top_val" != "val2" ]; then
+        printf "Top value incorrect: '%s' != val2.\n" "$top_val" >&2
         return 1
     elif [ "$(stack_len "stack")" -ne 2 ]; then
         printf "Stack length is incorrect after first pop: '%s' != 2.\n" \
@@ -85,8 +94,8 @@ test_stack_pop() {
     # Check for second pop.
     stack_pop "stack"
     top_val=$STACK_POP_RESULT
-    if [ "$top_val" != "2" ]; then
-        printf "Top value incorrect: '%s' != 2.\n" "$top_val" >&2
+    if [ "$top_val" != "val1" ]; then
+        printf "Top value incorrect: '%s' != val1.\n" "$top_val" >&2
         return 1
     elif [ "$(stack_len "stack")" -ne 1 ]; then
         printf "Stack length is incorrect after second pop: '%s' != 1.\n" \
@@ -97,8 +106,8 @@ test_stack_pop() {
     # Check for third pop.
     stack_pop "stack"
     top_val=$STACK_POP_RESULT
-    if [ "$top_val" != "1" ]; then
-        printf "Top value incorrect: '%s' != 1.\n" "$top_val" >&2
+    if [ "$top_val" != "val0" ]; then
+        printf "Top value incorrect: '%s' != val0.\n" "$top_val" >&2
         return 1
     elif [ "$(stack_len "stack")" -ne 0 ]; then
         printf "Stack length is incorrect after third pop: '%s' != 0.\n" \
@@ -109,90 +118,64 @@ test_stack_pop() {
 
 # Test stack_get.
 test_stack_get() {
-    # Setup stack with 3 values.
-    stack_create "stack"
-    stack_push "stack" "val1"
-    stack_push "stack" "val2"
-    stack_push "stack" "val3"
-
-    # Make sure the starting length is correct.
-    if [ "$(stack_len "stack")" -ne 3 ]; then
-        printf "Created stack does not have length 3.\n" >&2
-        return 1
-    fi
+    # Setup default stack.
+    _setup_test_stack "stack"
 
     # Check for the first value.
     val=$(stack_get "stack" "0")
-    if [ "$val" != "val1" ]; then
-        printf "The first value is incorrect: '%s' != val1.\n" "$val" >&2
+    if [ "$val" != "val0" ]; then
+        printf "The first value is incorrect: '%s' != val0.\n" "$val" >&2
         return 1
     fi
 
     # Check for the second value.
     val=$(stack_get "stack" "1")
-    if [ "$val" != "val2" ]; then
-        printf "The second value is incorrect: '%s' != val2.\n" "$val" >&2
+    if [ "$val" != "val1" ]; then
+        printf "The second value is incorrect: '%s' != val1.\n" "$val" >&2
         return 1
     fi
 
     # Check for the third value.
     val=$(stack_get "stack" "2")
-    if [ "$val" != "val3" ]; then
-        printf "The third value is incorrect: '%s' != val3.\n" "$val" >&2
+    if [ "$val" != "val2" ]; then
+        printf "The third value is incorrect: '%s' != val2.\n" "$val" >&2
         return 1
     fi
 }
 
 # Test stack_set.
 test_stack_set() {
-    # Setup stack with 3 values.
-    stack_create "stack"
-    stack_push "stack" "val1"
-    stack_push "stack" "val2"
-    stack_push "stack" "val3"
-
-    # Make sure the starting length is correct.
-    if [ "$(stack_len "stack")" -ne 3 ]; then
-        printf "Created stack does not have length 3.\n" >&2
-        return 1
-    fi
+    # Setup default stack.
+    _setup_test_stack "stack"
 
     # Make sure the 2nd value is correct.
     val=$(stack_get "stack" "1")
-    if [ "$val" != "val2" ]; then
-        printf "The second value is incorrect: '%s' != val2.\n" "$val" >&2
+    if [ "$val" != "val1" ]; then
+        printf "The second value is incorrect: '%s' != val1.\n" "$val" >&2
         return 1
     fi
 
     # Overwrite the 2nd value.
-    stack_set "stack" "1" "new2"
+    stack_set "stack" "1" "new1"
 
     # Check it has been properly overwritten.
     val=$(stack_get "stack" "1")
-    if [ "$val" != "new2" ]; then
-        printf "The overwritten value is incorrect: '%s' != 'new2'.\n" "$val" >&2
+    if [ "$val" != "new1" ]; then
+        printf \
+            "The overwritten value is incorrect: '%s' != 'new1'.\n" "$val" >&2
         return 1
     fi
 }
 
 # Test stack_delete.
 test_stack_delete() {
-    # Setup stack with 3 values.
-    stack_create "stack"
-    stack_push "stack" "val1"
-    stack_push "stack" "val2"
-    stack_push "stack" "val3"
-
-    # Make sure the starting length is correct.
-    if [ "$(stack_len "stack")" -ne 3 ]; then
-        printf "Created stack does not have length 3.\n" >&2
-        return 1
-    fi
+    # Setup default stack.
+    _setup_test_stack "stack"
 
     # Make sure the 2nd value is correct.
     val=$(stack_get "stack" "1")
-    if [ "$val" != "val2" ]; then
-        printf "The second value is incorrect: '%s' != val2.\n" "$val" >&2
+    if [ "$val" != "val1" ]; then
+        printf "The second value is incorrect: '%s' != val1.\n" "$val" >&2
         return 1
     fi
 
@@ -201,31 +184,22 @@ test_stack_delete() {
 
     # Check it has been properly overwritten.
     val=$(stack_get "stack" "1")
-    if [ "$val" != "val3" ]; then
-        printf "The value is incorrect after delete: '%s' != 'val3'.\n" "$val" >&2
+    if [ "$val" != "val2" ]; then
+        printf "The value is incorrect after delete: '%s' != 'val2'.\n" "$val" >&2
         return 1
     fi
 }
 
 # Test stack_foreach.
 test_stack_foreach() {
-    # Setup stack with 3 values.
-    stack_create "stack"
-    stack_push "stack" "val1"
-    stack_push "stack" "val2"
-    stack_push "stack" "val3"
-
-    # Make sure the starting length is correct.
-    if [ "$(stack_len "stack")" -ne 3 ]; then
-        printf "Created stack does not have length 3.\n" >&2
-        return 1
-    fi
+    # Setup default stack.
+    _setup_test_stack "stack"
 
     # Print all values.
     values=$(stack_foreach "stack" "printf '%s\n'")
-    expected_values="val1
-val2
-val3"
+    expected_values="val0
+val1
+val2"
     if [ "$values" != "$expected_values" ]; then
         printf "Printed values are not as expected.\n" >&2
         return 1
@@ -234,6 +208,49 @@ val3"
 
 # Test stack_destroy.
 test_stack_destroy() {
-    # TODO: Implement!
-    printf ""
+    # Setup default stack.
+    _setup_test_stack "stack"
+
+    # Check for correctly detecting the stack's length.
+    eval "stack_length=\${${STACK_PREFIX}stack_len}"
+    if [ "$stack_length" -ne 3 ]; then
+        printf "Wrongly detecting stack length." >&2
+        return 1
+    fi
+
+    # Check for correctly detecting the stack's items.
+    eval "value0=\${${STACK_PREFIX}stack_0}"
+    eval "value1=\${${STACK_PREFIX}stack_1}"
+    eval "value2=\${${STACK_PREFIX}stack_2}"
+    if [ "$value0" != "val0" ]; then
+        printf "Wrongly detecting value at index 0." >&2
+        return 1
+    elif [ "$value1" != "val1" ]; then
+        printf "Wrongly detecting value at index 1." >&2
+        return 1
+    elif [ "$value2" != "val2" ]; then
+        printf "Wrongly detecting value at index 2." >&2
+        return 1
+    fi
+
+    # Destroy stack and check if all variables have correctly been removed.
+    stack_destroy "stack"
+    eval "stack_length=\${${STACK_PREFIX}stack_len}"
+    eval "value0=\${${STACK_PREFIX}stack_0}"
+    eval "value1=\${${STACK_PREFIX}stack_1}"
+    eval "value2=\${${STACK_PREFIX}stack_2}"
+
+    if [ -n "$stack_length" ]; then
+        printf "Stack length not reset: '%s'." "$stack_length" >&2
+        return 1
+    elif [ -n "$value0" ]; then
+        printf "Stack value at index 0 did not reset: '%s'." "$value0" >&2
+        return 1
+    elif [ -n "$value1" ]; then
+        printf "Stack value at index 1 did not reset: '%s'." "$value1" >&2
+        return 1
+    elif [ -n "$value2" ]; then
+        printf "Stack value at index 2 did not reset: '%s'." "$value2" >&2
+        return 1
+    fi
 }
